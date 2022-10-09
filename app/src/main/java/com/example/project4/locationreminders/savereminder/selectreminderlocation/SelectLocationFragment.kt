11b09bcefd6796f0e5e9e531fa1f09f51262a74f
Mohamed El Masry <momamedalmasry@gmail.com>
2022-10-09 11:03:25 +0200

    setting up SelectLocationFragment.kt

--- app/src/main/java/com/example/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/example/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -1,21 +1,38 @@
 package com.example.project4.locationreminders.savereminder.selectreminderlocation
 
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.project4.R
 import com.example.project4.databinding.FragmentSelectLocationBinding
 import com.example.project4.base.BaseFragment
 import com.example.project4.locationreminders.savereminder.SaveReminderViewModel
 import com.example.project4.utils.setDisplayHomeAsUpEnabled
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import org.koin.android.ext.android.inject
+import kotlin.properties.Delegates
 
-class SelectLocationFragment : BaseFragment() {
+class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
 
     //Use Koin to get the view model of the SaveReminder
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSelectLocationBinding
+
+    private lateinit var map : GoogleMap
+    private val REQUEST_LOCATION_PERMISSION = 1
+    private val TAG = SelectLocationFragment::class.java.simpleName
+
+    private var userLong by Delegates.notNull<Double>()
+    private var userLat by Delegates.notNull<Double>()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -29,13 +46,16 @@ class SelectLocationFragment : BaseFragment() {
         setHasOptionsMenu(true)
         setDisplayHomeAsUpEnabled(true)
 
-//        TODO: add the map setup implementation
-//        TODO: zoom to the user location after taking his permission
-//        TODO: add style to the map
-//        TODO: put a marker to location that the user selected
+        // TODO: add the map setup implementation
+        val mapFragment = parentFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
+        // TODO: zoom to the user location after taking his permission
+        enableLocation()
+        // TODO: add style to the map
+        // TODO: put a marker to location that the user selected
 
-//        TODO: call this function after the user confirms on the selected location
+        // TODO: call this function after the user confirms on the selected location
         onLocationSelected()
 
         return binding.root
@@ -45,6 +65,13 @@ class SelectLocationFragment : BaseFragment() {
         //        TODO: When the user confirms on the selected location,
         //         send back the selected location details to the view model
         //         and navigate back to the previous fragment to save the reminder and add the geofence
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        val zoomLevel = 15f
+
     }
 
 
@@ -67,6 +94,40 @@ class SelectLocationFragment : BaseFragment() {
             true
         }
         else -> super.onOptionsItemSelected(item)
+    }
+
+    // Check that the location permission is granted
+    private fun isPermissionGranted(): Boolean = ContextCompat.checkSelfPermission(
+        this.requireContext(),
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    // enable the phone location permission
+    @SuppressLint("MissingPermission")
+    private fun enableLocation() {
+        if (isPermissionGranted()) {
+            map.isMyLocationEnabled = true
+        } else {
+            ActivityCompat.requestPermissions(
+                this.requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+        }
+    }
+
+    // CHeck the the permission result is granted
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == REQUEST_LOCATION_PERMISSION) {
+            if(grantResults.isNotEmpty() && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                enableLocation()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
 
