dc5e5af3bb877993df1e2777594e83fa788deee7
Mohamed El Masry <momamedalmasry@gmail.com>
2022-10-23 08:38:12 +0200

    Fake Test Data source

--- app/src/test/java/com/example/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/example/project4/locationreminders/data/FakeDataSource.kt
@@ -5,24 +5,36 @@ import com.example.project4.locationreminders.data.dto.ReminderDTO
 import com.example.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(var reminders : MutableList<ReminderDTO>? = mutableListOf()) : ReminderDataSource {
 
 //    TODO: Create a fake data source to act as a double to the real data source
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        reminders?.let {
+            return Result.Success(ArrayList(it))
+        }
+        return Result.Error("No Reminders found")
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminders?.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        val reminder = reminders?.find { it.id == id }
+        return try {
+            if(reminder == null) {
+                Result.Error("No reminder")
+            } else {
+                Result.Success(reminder)
+            }
+        } catch (ex : Exception) {
+            Result.Error(ex.localizedMessage)
+        }
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminders?.clear()
     }
 
 
