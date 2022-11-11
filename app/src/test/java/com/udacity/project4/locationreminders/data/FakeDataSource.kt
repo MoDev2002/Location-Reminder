c5e266f52e4fec08058e28474d7ce8ea6a046458
Mohamed El Masry <momamedalmasry@gmail.com>
2022-11-12 01:22:12 +0200

    changing fakeDataSource to match the LocalRepo

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,7 +4,7 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource(var reminders : MutableList<ReminderDTO>? = mutableListOf()) : ReminderDataSource {
+class FakeDataSource(var reminders : MutableList<ReminderDTO> = mutableListOf()) : ReminderDataSource {
 
     // Error Handling
     private var shouldReturnError = false
@@ -14,24 +14,25 @@ class FakeDataSource(var reminders : MutableList<ReminderDTO>? = mutableListOf()
     }
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        if(shouldReturnError) {
-            return Result.Error("Test Exception")
+        return try {
+            if (shouldReturnError) {
+                throw Exception("Reminders not found")
+            }
+            Result.Success(ArrayList(reminders))
+        } catch (ex : Exception) {
+            Result.Error(ex.localizedMessage)
         }
-        reminders?.let {
-            return Result.Success(ArrayList(it))
-        }
-        return Result.Error("No Reminders found")
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        reminders?.add(reminder)
+        reminders.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        val reminder = reminders?.find { it.id == id }
         return try {
+            val reminder = reminders.find { it.id == id }
             if(reminder == null || shouldReturnError) {
-                Result.Error("No reminder")
+                Result.Error("Reminder not found!")
             } else {
                 Result.Success(reminder)
             }
@@ -41,7 +42,7 @@ class FakeDataSource(var reminders : MutableList<ReminderDTO>? = mutableListOf()
     }
 
     override suspend fun deleteAllReminders() {
-        reminders?.clear()
+        reminders.clear()
     }
 
 
