7817be66b30a10860fdbb3baacab8a6520521eb8
Mohamed El Masry <momamedalmasry@gmail.com>
2022-11-13 23:57:38 +0200

    Changing auth flow of the app

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -25,16 +25,18 @@ class AuthenticationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_authentication)
-        // TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
-        // TODO: If the user was authenticated, send him to RemindersActivity
+
+       viewModel.authenticationState.observe(this) { authState ->
+            if (authState == AuthenticationViewModel.AuthState.AUTHENTICATED) {
+                navigateRemindersActivity()
+            }
+        }
 
         findViewById<Button>(R.id.loginBtn).setOnClickListener {
             viewModel.authenticationState.observe(this) { authState ->
                 authStateCheck(authState)
             }
         }
-        // TODO: a bonus is to customize the sign in flow to look nice using :
-        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
 
     }
 
@@ -42,11 +44,15 @@ class AuthenticationActivity : AppCompatActivity() {
         when(authState) {
             AuthenticationViewModel.AuthState.UNAUTHENTICATED -> launchSignInFlow()
             AuthenticationViewModel.AuthState.AUTHENTICATED -> {
-                val intent = Intent(this, RemindersActivity::class.java)
-                startActivity(intent)
-                finish()
+                navigateRemindersActivity()
             }
         }
+    }
+
+    private fun navigateRemindersActivity() {
+        val intent = Intent(this, RemindersActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 
     private fun launchSignInFlow() {
